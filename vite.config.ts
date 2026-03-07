bc3e15727852741b508cc2959c2b73241e6db190
INnoVatioN-97 <koyoungil97@gmail.com>
2026-03-07 16:41:25 +0900

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    server: {
+        port: 5173,
+        proxy: {
+            // 백엔드 API 프록시 설정
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+        },
+    },
+});
